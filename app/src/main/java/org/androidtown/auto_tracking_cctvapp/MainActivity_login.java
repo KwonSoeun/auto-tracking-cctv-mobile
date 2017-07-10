61d400eb6c19354990dbbf494017cf569cd46728
KwonSoeun <ksu5128@naver.com>
2017-07-10 15:50:15 +0900

    Initialized Android Project
    UI만

---
+++ app/src/main/java/org/androidtown/auto_tracking_cctvapp/MainActivity_login.java
@@ -0,0 +1,13 @@
+package org.androidtown.auto_tracking_cctvapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_server_connect);
+    }
+}
