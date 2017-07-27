2c3a97d08b9e3b4b5aebff1d38a7cc65b1f6834a
KwonSoeun <ksu5128@naver.com>
2017-07-27 18:54:15 +0900

    decoding/incoding test

--- app/src/main/java/org/androidtown/auto_tracking_cctvapp/send_command_to_web_server.java
+++ app/src/main/java/org/androidtown/auto_tracking_cctvapp/send_command_to_web_server.java
@@ -2,15 +2,20 @@ package org.androidtown.auto_tracking_cctvapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import org.androidtown.auto_tracking_cctvapp.retrofit.Direction;
 import org.androidtown.auto_tracking_cctvapp.retrofit.RetrofitService;
 import org.androidtown.auto_tracking_cctvapp.retrofit.direction_message;
 import org.androidtown.auto_tracking_cctvapp.server_connect.server_ip_port_camera;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 import retrofit2.Call;
@@ -25,6 +30,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class send_command_to_web_server extends Activity {
 
+    ImageView image_view;
     ImageButton up_btn, left_btn, right_btn, down_btn;
 
     server_ip_port_camera server_ip_port_camera; //server info object(ip address, port num)
@@ -45,42 +51,59 @@ public class send_command_to_web_server extends Activity {
         port_num = server_ip_port_camera.get_port_num();
         camera_num = server_ip_port_camera.get_camera_num();
 
-        //ImageButton Setting
+        //Setting
+        image_view = (ImageView) findViewById(R.id.ImageView);
+
         up_btn = (ImageButton) findViewById(R.id.up_button);
         left_btn = (ImageButton) findViewById(R.id.left_button);
         right_btn = (ImageButton) findViewById(R.id.right_button);
         down_btn = (ImageButton) findViewById(R.id.down_button);
 
+
+        //test for encoding image to base64 string
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ryan);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes1 = baos.toByteArray();
+        String imageString = Base64.encodeToString(imageBytes1, Base64.DEFAULT);
+
+        //test for decoding base64 string to image
+        byte[] imageBytes2 = Base64.decode(imageString, Base64.DEFAULT);
+        Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes2, 0, imageBytes2.length);
+        image_view.setImageBitmap(decodedImage);
+
+
         //ImageButton Event
         up_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                send_command("up");  //서버에 "up" 보내기
+                send_command("UP");  //서버에 "up" 보내기
             }
         });
 
         left_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                send_command("left");  //서버에 "left" 보내기
+                send_command("LEFT");  //서버에 "left" 보내기
             }
         });
 
         right_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                send_command("right");  //서버에 "right" 보내기
+                send_command("RIGHT");  //서버에 "right" 보내기
             }
         });
 
         down_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                send_command("down");  //서버에 "down" 보내기
+                send_command("DOWN");  //서버에 "down" 보내기
             }
         });
     }
 
+    //retrofit http
     public void send_command(String direction) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://" + ip_address + ":" + port_num +"/")
