924c06c0c21789715d06b34821356b3797df8fb3
KwonSoeun <ksu5128@naver.com>
2017-08-20 04:01:05 +0900

    fcm : send message from console

--- app/src/main/java/org/androidtown/auto_tracking_cctvapp/fcm/MyFirebaseInstanceIdService.java
+++ app/src/main/java/org/androidtown/auto_tracking_cctvapp/fcm/MyFirebaseInstanceIdService.java
@@ -5,13 +5,22 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
 /**
  * Created by soeun on 2017-08-17.
  */
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService{
-    private  final static String TAG = "FCM_ID";
+    private final static String TAG = "MyFirebaseIIdService";
 
+    @Override
     public void onTokenRefresh() {
+//        super.onTokenRefresh();
         String token = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + token);
 
@@ -19,11 +28,22 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService{
     }
 
     private void sendRegistrationToServer(String token) {
-//        OkHttpClient client = new OkHttpClient();
-//        RequestBody body = new FormBody().Builder()
-//                .add("Token", token)
-//                .build();
-    }
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("Token",token)
+                .build();
 
+        //request
+        Request request = new Request.Builder()
+                .url("http://164.125.68.74/fcm/register") //수정
+                .post(body)
+                .build();
+
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
