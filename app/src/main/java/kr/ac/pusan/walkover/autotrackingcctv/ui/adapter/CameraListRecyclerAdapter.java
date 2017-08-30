b389b73d90800e98960d7b34e4b3af84174a081b
gihwan <0x1306e6d@gmail.com>
2017-08-30 14:33:19 +0900

    Design camera card's layout

--- app/src/main/java/kr/ac/pusan/walkover/autotrackingcctv/ui/adapter/CameraListRecyclerAdapter.java
+++ app/src/main/java/kr/ac/pusan/walkover/autotrackingcctv/ui/adapter/CameraListRecyclerAdapter.java
@@ -1,12 +1,18 @@
 package kr.ac.pusan.walkover.autotrackingcctv.ui.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+import kr.ac.pusan.walkover.autotrackingcctv.R;
 
 public class CameraListRecyclerAdapter extends RecyclerView.Adapter<CameraListRecyclerViewHolder> {
     @Override
     public CameraListRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.camera_card, parent, false);
+        return new CameraListRecyclerViewHolder(view);
     }
 
     @Override
