fe65d8eb7a8bf0ed5e90940e0da34c42a12c4970
Unknown <ksu5128@naver.com>
2017-08-30 21:28:17 +0900

    fcm- send token

---
+++ app/src/main/java/kr/ac/pusan/walkover/autotrackingcctv/retrofit/fcm_Token.java
@@ -0,0 +1,16 @@
+package kr.ac.pusan.walkover.autotrackingcctv.retrofit;
+
+/**
+ * Created by kwonsoeun on 2017-08-25.
+ */
+
+public class fcm_Token {
+    //private Integer camera_num;
+    private String token;
+
+    public fcm_Token(String token) {
+      //  this.camera_num = camera_num;
+        this.token = token;
+    }
+
+}
