10c8ecb3c6073cdf8fbded56dbe47607f9f48451
gihwan <0x1306e6d@gmail.com>
2017-08-30 13:47:18 +0900

    Design MainActivity's layout
    Start MainActivity when LoginActivity's connect button is clicked.
    Constants are supervised by AutoTrackingCCTVConstants

---
+++ app/src/main/java/kr/ac/pusan/walkover/autotrackingcctv/AutoTrackingCCTVConstants.java
@@ -0,0 +1,13 @@
+package kr.ac.pusan.walkover.autotrackingcctv;
+
+public final class AutoTrackingCCTVConstants {
+
+    public static final int DEFAULT_PORT = 9090;
+
+    public static final String IP_ADDRESS_KEY = "ip-address";
+    public static final String PORT_KEY = "port";
+
+    private AutoTrackingCCTVConstants() {
+//        nothing
+    }
+}
--- app/src/main/java/kr/ac/pusan/walkover/autotrackingcctv/ui/MainActivity.java
+++ app/src/main/java/kr/ac/pusan/walkover/autotrackingcctv/ui/MainActivity.java
@@ -2,14 +2,78 @@ package kr.ac.pusan.walkover.autotrackingcctv.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import kr.ac.pusan.walkover.autotrackingcctv.AutoTrackingCCTVConstants;
 import kr.ac.pusan.walkover.autotrackingcctv.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private String mIpAddress;
+    private int mPort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+
+        setupToolbar();
+        getExtra();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.main_menu_action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            // TODO: 2017-08-30 configure support action bar
+        }
+    }
+
+    private void getExtra() {
+        mIpAddress = getIntent().getStringExtra(AutoTrackingCCTVConstants.IP_ADDRESS_KEY);
+        mPort = getIntent().getIntExtra(AutoTrackingCCTVConstants.PORT_KEY, AutoTrackingCCTVConstants.DEFAULT_PORT);
+        Log.d(TAG, "getExtra() called. ipAddress=" + mIpAddress + ", port=" + mPort);
     }
 }
